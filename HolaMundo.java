7bcb4befe0c0d9868b431a7cb72cf7bde20785f1
Rafael Fernandez Villaca <rafaelfernandezvillaca@gmail.com>
2018-04-26 22:56:10 +0200

    se agregaron los archivos

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+/**
+*Clase de ejemplocon java
+*author rafael
+* osince 1.0.0
+*/
+public class HolaMundo {  
+  /**Metodo estatico que ejecuta el programa
+   */
+  public static void main (String[] args) {
+    System.out.println("Hola Mundo");
+   }
+}
